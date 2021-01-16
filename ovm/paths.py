331f8aa25fce35e6083d6b8a38c7403bdbb2e6d4
Michael Nowotny <nowotnym@gmail.com>
2021-01-15 21:11:31 -0800

    - moved historical data from 'notebooks' to 'historical_data'

--- ovm/paths.py
+++ ovm/paths.py
@@ -5,5 +5,5 @@ import ovm
 
 
 BASE_DIRECTORY = str(Path(os.path.dirname(ovm.__file__)).parents[0])
-HISTORICAL_DATA_DIRECTORY = os.path.join(BASE_DIRECTORY, 'notebooks')
+HISTORICAL_DATA_DIRECTORY = os.path.join(BASE_DIRECTORY, 'historical_data')
 SIMULATED_DATA_DIRECTORY = os.path.join(BASE_DIRECTORY, 'monetary', 'sims')
