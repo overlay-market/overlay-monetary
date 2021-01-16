63d1cd6b8162042357a5278077cedabe97a0ea04
Michael Nowotny <nowotnym@gmail.com>
2021-01-15 21:24:55 -0800

    - removed syntax error in old inflation.py

--- abm_old/inflation.py
+++ abm_old/inflation.py
@@ -9,18 +9,18 @@ from model import OVLModel
 from collections import defaultdict
 
 
-
 base = 25e6
 per_min = np.array([10 - x for x in range(10)])
 per_day = 1440*per_min
 per_year = 365*per_day
-supply = 
+# supply =
 inflation = per_year
 
-def main():
 
+def main():
 	supply = [base+per_year*x for x in range(10)]
 	inflation = [0] + [supply[i+1]/supply[i] - 1 for i in range(len(supply) - 1)] 
+
 
 if __name__ == '__main__':
 	if len(sys.argv) > 1:
