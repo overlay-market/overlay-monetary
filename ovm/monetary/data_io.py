a8186f353e5fb9f9868f4b3ec697a6996e5ab217
michael <ryce-mek>
2021-01-29 21:24:47 -0800

    - added class AgentBasedSimulationInputData to serve as a wrapper for sims map and associated metadata to be transported to data collector and written to the HDF5 file

--- ovm/monetary/data_io.py
+++ ovm/monetary/data_io.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import os
 import typing as tp
 import numpy as np
@@ -25,6 +26,24 @@ from ovm.time_resolution import TimeResolution
 FILE_EXTENSION = 'parq'
 
 
+@dataclass(frozen=True)
+class AgentBasedSimulationInputData:
+    ticker_to_series_of_prices_map: tp.Dict[str, np.ndarray]
+    time_resolution: TimeResolution
+    historical_data_source: HistoricalDataSource
+    ovl_ticker: str
+    ovl_quote_ticker: str
+    numpy_seed: tp.Optional[int] = None  # this is the same as data_sim_rng
+
+    @property
+    def is_resampled(self) -> bool:
+        return self.numpy_seed is not None
+
+    @property
+    def tickers(self) -> tp.Tuple[str]:
+        return tuple(self.ticker_to_series_of_prices_map.keys())
+
+
 def convert_to_parq(csv_path: str):
     """Converts a csv file to parquet"""
     root, csv_ext = os.path.splitext(csv_path)
@@ -34,6 +53,7 @@ def convert_to_parq(csv_path: str):
         data.to_parquet(parq_path)
 
 
+# ToDo: Merge this function into construct_abs_data_input_from_resampled_data when all call sites have switched
 def construct_sims_map(
         data_sim_rng: int,
         time_resolution: TimeResolution,
@@ -68,6 +88,34 @@ def construct_sims_map(
     return ticker_to_time_series_of_prices_map
 
 
+def construct_abs_data_input_from_resampled_data(
+        data_sim_rng: int,
+        time_resolution: TimeResolution,
+        historical_data_source: HistoricalDataSource,
+        tickers: tp.Sequence[str],
+        # for sim source, since OVL doesn't actually exist yet
+        ovl_ticker: str = YFI_USD_TICKER,
+        ovl_quote_ticker: str = OVL_USD_TICKER,
+        verbose: bool = False) -> AgentBasedSimulationInputData:
+    ticker_to_time_series_of_prices_map = \
+        construct_sims_map(data_sim_rng=data_sim_rng,
+                           time_resolution=time_resolution,
+                           historical_data_source=historical_data_source,
+                           tickers=tickers,
+                           ovl_ticker=ovl_ticker,
+                           ovl_quote_ticker=ovl_quote_ticker,
+                           verbose=verbose)
+
+    return AgentBasedSimulationInputData(
+            ticker_to_series_of_prices_map=ticker_to_time_series_of_prices_map,
+            time_resolution=time_resolution,
+            historical_data_source=historical_data_source,
+            ovl_ticker=ovl_ticker,
+            ovl_quote_ticker=ovl_quote_ticker,
+            numpy_seed=data_sim_rng)
+
+
+# ToDo: Merge this function into construct_abs_data_input_from_historical_data when all call sites have switched
 def construct_hist_map(
         time_resolution: TimeResolution,
         historical_data_source: HistoricalDataSource,
@@ -96,6 +144,29 @@ def construct_hist_map(
             close_prices[ticker] = f.transpose().values.reshape((-1, ))
 
     return close_prices
+
+
+def construct_abs_data_input_from_historical_data(
+        time_resolution: TimeResolution,
+        historical_data_source: HistoricalDataSource,
+        tickers: tp.Sequence[str],
+        ovl_ticker: str = YFI_USD_TICKER,
+        ovl_quote_ticker: str = OVL_USD_TICKER,
+        verbose: bool = False) -> AgentBasedSimulationInputData:
+    ticker_to_series_of_prices_map = \
+        construct_hist_map(time_resolution=time_resolution,
+                           historical_data_source=historical_data_source,
+                           tickers=tickers,
+                           ovl_ticker=ovl_ticker,
+                           ovl_quote_ticker=ovl_quote_ticker,
+                           verbose=verbose)
+    return AgentBasedSimulationInputData(
+            ticker_to_series_of_prices_map=ticker_to_series_of_prices_map,
+            time_resolution=time_resolution,
+            historical_data_source=historical_data_source,
+            ovl_ticker=ovl_ticker,
+            ovl_quote_ticker=ovl_quote_ticker,
+            numpy_seed=None)
 
 
 def construct_ticker_to_series_of_prices_map_from_simulated_prices(
